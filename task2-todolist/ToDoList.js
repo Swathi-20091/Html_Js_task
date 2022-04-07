bdd708a171ca553239357c5404e61d97e560cde2
shwathi-20091 <103167133+shwathi-20091@users.noreply.github.com>
2022-04-07 13:06:00 +0530

    Update ToDoList.js

--- task2-todolist/ToDoList.js
+++ task2-todolist/ToDoList.js
@@ -96,7 +96,7 @@ function check(){
 
     var userName = document.getElementById('userName');
     var userPw = document.getElementById('userPw');
-    var userRemember = document.getElementById("rememberMe");
+    
 
     if(userName.value == storedName && userPw.value == storedPw){
         alert('You have logged in');
