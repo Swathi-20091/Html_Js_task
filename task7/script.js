40bf128f444d89df38719d6f1fa2646e94428bf5
shwathi-20091 <103167133+shwathi-20091@users.noreply.github.com>
2022-04-07 14:49:13 +0530

    Update script.js

--- task7/script.js
+++ task7/script.js
@@ -1,7 +1,7 @@
 function searchFun(){
     var key=document.querySelector("#searchBox").value.toUpperCase();
     var myItems=document.querySelectorAll(".column");
-    for(let i=0;i<myItems.length;i++){
+    for(let i of myItems){
       var inItems=myItems[i].querySelector("figcaption").innerText.toUpperCase();
       if(inItems.indexOf(key)>-1){
         myItems[i].style.display=""
