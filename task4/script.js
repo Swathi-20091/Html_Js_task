93c57dc39c2783107805783d4f491b6543b276cf
shwathi-20091 <103167133+shwathi-20091@users.noreply.github.com>
2022-04-18 11:07:22 +0530

    Update script.js

--- task4/script.js
+++ task4/script.js
@@ -159,7 +159,7 @@ function onEdit(td){
     document.getElementById('phonenumber').value = selectedRow.cells[6].innerHTML;
 }
 //update the data
-function updateRecord(formData){
+function updateRecord(){
     selectedRow.cells[0].innerHTML = formData.username;
     selectedRow.cells[1].innerHTML = formData.DOB;
     selectedRow.cells[2].innerHTML = formData.Age;
